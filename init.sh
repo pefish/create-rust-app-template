fcf08f87bcf5ff90e8ac02d0b4d06331129c6e2b
pefish <pefish@qq.com>
2022-03-30 18:00:28 +0800

    add

--- init.sh
+++ init.sh
@@ -2,8 +2,6 @@
 
 cat Cargo.toml | sed "s/template/${NAME}/g" > temp && rm -rf Cargo.toml && mv temp Cargo.toml
 
-cat Cargo.toml | sed "s/_description_/${DESC}/g" > temp && rm -rf Cargo.toml && mv temp Cargo.toml
-
 cargo update
 
-
+rm -rf ./init.sh
