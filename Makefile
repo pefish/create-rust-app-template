47460d569f02518d3fd1ed30a3eba6cb8e1b7bb7
pefish <pefish@qq.com>
2022-03-30 17:17:05 +0800

    add

---
+++ Makefile
@@ -0,0 +1,25 @@
+
+
+DEFAULT: build-release
+
+build-release:
+	cargo +nightly build --release
+
+build:
+	cargo +nightly build
+
+run:
+	RUST_LOG=debug RUST_BACKTRACE=1 RUST_CONFIG=./config/sample.toml cargo +nightly run
+
+run-bin:
+	RUST_LOG=debug RUST_BACKTRACE=1 RUST_CONFIG=./config/sample.toml cargo +nightly run --bin $(bin)
+
+install: build-release
+	sudo install -C ./target/release/lotus-window-client /usr/local/bin/lotus-window-client-rs
+
+install-service: install
+	sudo mkdir -p /etc/systemd/system
+	sudo install -C -m 0644 ./script/lotus-window-client-rs.service /etc/systemd/system/lotus-window-client-rs.service
+	sudo systemctl daemon-reload
+	@echo
+	@echo "lotus-window-client service installed."
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
 
-fn main() {
-    println!("{}", "hello");
+use tokio::{fs::File, io::AsyncReadExt};
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Config {
+    pub test: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+
+    let config_env = std::env::var("RUST_CONFIG")?;
+
+    let mut fs = File::open(config_env).await?;
+    let mut config_str = String::new();
+    fs.read_to_string(&mut config_str).await?;
+
+    log::debug!("config: {}", config_str);
+
+    let config: Config = toml::from_str(config_str.as_str())?;
+    log::info!("config.test: {:?}", config.test);
+    
+    Ok(())
 }
